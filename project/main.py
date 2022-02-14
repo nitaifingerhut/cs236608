43d196715d662be44979c9157b6154fdfbc19c9d
manorz <manor.zvi@campus.technion.ac.il>
2022-02-14 08:35:01 +0200

    ...

--- project/main.py
+++ project/main.py
@@ -1,3 +1,7 @@
+from asyncio.log import logger
+from lib2to3.pgen2.pgen import DFAState
+from ntpath import join
+import os
 import argparse
 import imageio
 import matplotlib.pyplot as plt
@@ -20,11 +24,33 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--num-items", type=int, default=100)
     parser.add_argument("--num-topics", type=int, default=10)
     parser.add_argument("--rating-freq", type=float, default=0.2)
+    parser.add_argument("--res-dir", type=str)
+    parser.add_argument("--env-type", type=str, choices=('dynamic', 'dynamic-reverse'), default='dynamic')
+    parser.add_argument("--exp-repeats", type=int, default=1)
+    # parser.add_argument("--env-topic-change", nargs='+', type=float)  # For some weird bug in nargs='+' only in VsCode... (manorz, 02/12/22)
+    parser.add_argument("--env-topic-change", type=str, default='0,1,2')
+    parser.add_argument("--rec-eps-greedy", type=float)
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     opts = parse_args()
+    try:
+        opts.res_dir = os.path.join('results', opts.res_dir)
+    except TypeError as _:
+        opts.res_dir = os.path.join('results', opts.env_type, opts.recommender)
+        if opts.rec_eps_greedy:
+            opts.res_dir = os.path.join(opts.res_dir, f'eps_greedy={opts.rec_eps_greedy}')
+    opts.res_dir = os.path.abspath(opts.res_dir)
+    try:
+        os.makedirs(opts.res_dir)
+    except FileExistsError as e:
+        logger.warning(e)
+        os.makedirs(opts.res_dir, exist_ok=True)
+    if opts.rec_eps_greedy:
+        opts.rec_eps_greedy = {'type': 'eps_greedy', 'eps': opts.rec_eps_greedy}
+    
+    opts.env_topic_change = [float(x) for x in opts.env_topic_change.split(',')]
 
     env_params = {
         "num_topics": opts.num_topics,
@@ -33,19 +59,9 @@ if __name__ == "__main__":
         "rating_frequency": opts.rating_freq,
         "num_init_ratings": opts.num_users * opts.num_items // 5,
         "noise": 0.0,
-        "user_model": "rebelling",
+        "user_model": opts.env_type,
     }
-    env = Topics(**env_params)
-    env.seed(opts.seed)
-
-    # LibFM_params = {
-    #     "num_user_features": 0,
-    #     "num_item_features": 0,
-    #     "num_rating_features": 0,
-    #     "max_num_users": opts.num_users,
-    #     "max_num_items": opts.num_items,
-    #     "seed": opts.seed,
-    # }
+    
     Autorec_params = {
         "num_users": opts.num_users,
         "num_items": opts.num_items,
@@ -53,39 +69,17 @@ if __name__ == "__main__":
         "train_epoch": 100,
         "random_seed": opts.seed,
     }
+
     recommender = RECOMMENDERS[opts.recommender](**Autorec_params)
 
+    if opts.rec_eps_greedy:
+        recommender.update_strategy(opts.rec_eps_greedy)
+
     callbacks_kwargs = dict(user_id=0)
-    callbacks = [RMSE_func, ARRI_func, user_preferences]
+    callbacks = [RMSE_func, ARRI_func]#, user_preferences]
 
-    # env = Topics(**env_params, topic_change=1)
-    # RMSEs, ARRIs, PREFERENCES = run_simulation(
-    #     env=env,
-    #     recommender=recommender,
-    #     steps=opts.steps,
-    #     rpu=1,
-    #     retrain=True,
-    #     callbacks=callbacks,
-    #     callbacks_kwargs=callbacks_kwargs,
-    #     reset=True,
-    #     seed=opts.seed,
-    # )
-    # plot_graphs(RMSEs, title="rmse")
-    # plot_graphs(ARRIs, title="arri")
-    #
-    # vid_writer = imageio.get_writer("preferences.mp4", fps=5)
-    # PREFERENCES = np.stack(PREFERENCES, axis=0)
-    # bins = np.linspace(math.floor(np.min(PREFERENCES)), math.ceil(np.max(PREFERENCES)) + 1, num=10)
-    # for x in PREFERENCES:
-    #     _, ax = plt.subplots()
-    #     ax.hist(x, bins=bins)
-    #     data = plt_to_numpy(ax)
-    #     plt.close()
-    #     vid_writer.append_data(data)
-    # vid_writer.close()
-
-    repeats = 1
-    topic_changes = [1]
+    repeats = opts.exp_repeats
+    topic_changes = opts.env_topic_change
     res = run_experiment(
         env_params=env_params,
         recommender=recommender,
@@ -99,4 +93,4 @@ if __name__ == "__main__":
         topic_change=topic_changes,
     )
     for k, v in res.items():
-        plot_graphs(*list(v.values()), title=k, legend=True, labels=[f"topic_change={x}" for x in topic_changes])
+        plot_graphs(*list(v.values()), title=k, legend=True, labels=[f"topic_change={x}" for x in topic_changes], save=os.path.join(opts.res_dir, k))
