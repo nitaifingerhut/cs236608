32ea7fc9baaf9d82e947ac1b18ec13c19ee46a72
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2022-03-07 22:36:24 +0200

    scripts

--- project/scripts/run_eg_0_00.sh
+++ project/scripts/run_eg_0_00.sh
@@ -5,15 +5,9 @@ source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate double-ml
 echo "hello from $(python --version) in $(which python)"
 
-python -m main --steps 500 --recommender temporal_autorec --rats-init-mode zeros   --recs-init-mode zeros    --temporal-window-size 1 --rec-eps-greedy 0.0 --res-dir st_500_ta_rat_0_rec_0_tw_1_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec --rats-init-mode zeros   --recs-init-mode zeros    --temporal-window-size 3 --rec-eps-greedy 0.0 --res-dir st_500_ta_rat_0_rec_0_tw_3_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec --rats-init-mode zeros   --recs-init-mode zeros    --temporal-window-size 5 --rec-eps-greedy 0.0 --res-dir st_500_ta_rat_0_rec_0_tw_5_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec --rats-init-mode randint --recs-init-mode randint  --temporal-window-size 1 --rec-eps-greedy 0.0 --res-dir st_500_ta_rat_r_rec_r_tw_1_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec --rats-init-mode randint --recs-init-mode randint  --temporal-window-size 3 --rec-eps-greedy 0.0 --res-dir st_500_ta_rat_r_rec_r_tw_3_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec --rats-init-mode randint --recs-init-mode randint  --temporal-window-size 5 --rec-eps-greedy 0.0 --res-dir st_500_ta_rat_r_rec_r_tw_5_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec2 --rats-init-mode zeros   --recs-init-mode zeros   --temporal-window-size 1 --rec-eps-greedy 0.0 --res-dir st_500_ta2_rat_0_rec_0_tw_1_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec2 --rats-init-mode zeros   --recs-init-mode zeros   --temporal-window-size 3 --rec-eps-greedy 0.0 --res-dir st_500_ta2_rat_0_rec_0_tw_3_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec2 --rats-init-mode zeros   --recs-init-mode zeros   --temporal-window-size 5 --rec-eps-greedy 0.0 --res-dir st_500_ta2_rat_0_rec_0_tw_5_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec2 --rats-init-mode randint --recs-init-mode randint --temporal-window-size 1 --rec-eps-greedy 0.0 --res-dir st_500_ta2_rat_r_rec_r_tw_1_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec2 --rats-init-mode randint --recs-init-mode randint --temporal-window-size 3 --rec-eps-greedy 0.0 --res-dir st_500_ta2_rat_r_rec_r_tw_3_eg_0_0
-python -m main --steps 500 --recommender temporal_autorec2 --rats-init-mode randint --recs-init-mode randint --temporal-window-size 5 --rec-eps-greedy 0.0 --res-dir st_500_ta2_rat_r_rec_r_tw_5_eg_0_0
+python -m main --env-type dynamic                           --rec-eps-greedy 0.0 --res-dir env_d_eg_0_0
+python -m main --env-type dynamic-reverse                   --rec-eps-greedy 0.0 --res-dir env_dr_eg_0_0
+python -m main --env-type dynamic-rec-agnostic              --rec-eps-greedy 0.0 --res-dir env_dra_eg_0_0
+python -m main --env-type dynamic-rec-agnostic-rand         --rec-eps-greedy 0.0 --res-dir env_drar_eg_0_0
+python -m main --env-type dynamic-reverse-rec-agnostic      --rec-eps-greedy 0.0 --res-dir env_drra_eg_0_0
+python -m main --env-type dynamic-reverse-rec-agnostic-rand --rec-eps-greedy 0.0 --res-dir env_drrar_eg_0_0
