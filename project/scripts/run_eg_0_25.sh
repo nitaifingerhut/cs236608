4646b5b7da66f045895c3ffc520b3c98fd5ecdb8
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2022-03-08 00:48:55 +0200

    ...

--- project/scripts/run_eg_0_25.sh
+++ project/scripts/run_eg_0_25.sh
@@ -5,9 +5,9 @@ source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate double-ml
 echo "hello from $(python --version) in $(which python)"
 
-python -m main --env-type dynamic                           --rec-eps-greedy 0.25 --res-dir env_d_eg_0_25
-python -m main --env-type dynamic-reverse                   --rec-eps-greedy 0.25 --res-dir env_dr_eg_0_25
-python -m main --env-type dynamic-rec-agnostic              --rec-eps-greedy 0.25 --res-dir env_dra_eg_0_25
-python -m main --env-type dynamic-rec-agnostic-rand         --rec-eps-greedy 0.25 --res-dir env_drar_eg_0_25
-python -m main --env-type dynamic-reverse-rec-agnostic      --rec-eps-greedy 0.25 --res-dir env_drra_eg_0_25
-python -m main --env-type dynamic-reverse-rec-agnostic-rand --rec-eps-greedy 0.25 --res-dir env_drrar_eg_0_25
+python -m main --env-type dynamic                           --exp-repeats 10 --rec-eps-greedy 0.25 --res-dir env_d_eg_0_25
+python -m main --env-type dynamic-reverse                   --exp-repeats 10 --rec-eps-greedy 0.25 --res-dir env_dr_eg_0_25
+python -m main --env-type dynamic-rec-agnostic              --exp-repeats 10 --rec-eps-greedy 0.25 --res-dir env_dra_eg_0_25
+python -m main --env-type dynamic-rec-agnostic-rand         --exp-repeats 10 --rec-eps-greedy 0.25 --res-dir env_drar_eg_0_25
+python -m main --env-type dynamic-reverse-rec-agnostic      --exp-repeats 10 --rec-eps-greedy 0.25 --res-dir env_drra_eg_0_25
+python -m main --env-type dynamic-reverse-rec-agnostic-rand --exp-repeats 10 --rec-eps-greedy 0.25 --res-dir env_drrar_eg_0_25
