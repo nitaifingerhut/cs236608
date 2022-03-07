06ae8cba5b89443bd4023952e01f1db95b445340
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2022-03-07 22:48:18 +0200

    ...

--- project/scripts/run.sh
+++ project/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sbatch -c 2 -o run_eg_0_00.out -J r00 run_eg_0_00.sh
-sbatch -c 2 -o run_eg_0_15.out -J r01 run_eg_0_25.sh
-sbatch -c 2 -o run_eg_0_30.out -J r02 run_eg_0_50.sh
+sbatch -c 2 -o run_eg_0_25.out -J r01 run_eg_0_25.sh
+sbatch -c 2 -o run_eg_0_50.out -J r02 run_eg_0_50.sh
