5c6cc2f4dbbdaf8da979f1cfc5e3d4063938df6b
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2022-03-12 22:06:02 +0200

    more exps

---
+++ project/scripts/run_ta.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ..
+source $HOME/miniconda3/etc/profile.d/conda.sh
+conda activate double-ml
+echo "hello from $(python --version) in $(which python)"
+
+python -m main --env-type dynamic-reverse              --exp-repeats 10 --recommender temporal_autorec --temporal-window-size 1 --res-dir env_dr_rec_ta_1
+python -m main --env-type dynamic-reverse-rec-agnostic --exp-repeats 10 --recommender temporal_autorec --temporal-window-size 1 --res-dir env_drra_rec_ta_1
+python -m main --env-type dynamic-reverse              --exp-repeats 10 --recommender temporal_autorec --temporal-window-size 3 --res-dir env_dr_rec_ta_3
+python -m main --env-type dynamic-reverse-rec-agnostic --exp-repeats 10 --recommender temporal_autorec --temporal-window-size 3 --res-dir env_drra_rec_ta_3
+python -m main --env-type dynamic-reverse              --exp-repeats 10 --recommender temporal_autorec --temporal-window-size 5 --res-dir env_dr_rec_ta_5
+python -m main --env-type dynamic-reverse-rec-agnostic --exp-repeats 10 --recommender temporal_autorec --temporal-window-size 5 --res-dir env_drra_rec_ta_5
